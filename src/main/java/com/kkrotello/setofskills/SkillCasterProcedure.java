284e1bfb0129bdf8a538f3f0136dadf796d2db40
Kkrotello <wijayakenjip@gmail.com>
2024-02-24 16:21:56 +0700

    Skill works!

--- src/main/java/com/kkrotello/setofskills/SkillCasterProcedure.java
+++ src/main/java/com/kkrotello/setofskills/SkillCasterProcedure.java
@@ -1,11 +1,35 @@
 package com.kkrotello.setofskills;
 
+import com.kkrotello.setofskills.skill.AirJump;
+import net.minecraft.client.resources.language.I18n;
 import net.minecraft.network.chat.Component;
+import net.minecraft.world.InteractionHand;
 import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.Item;
 import net.minecraft.world.level.Level;
 
 public class SkillCasterProcedure {
     public static void execute (Level world, Player user, double x, double y, double z){
-        user.displayClientMessage(Component.literal("Test test"), true);
+        if (user == null)
+            return;
+        if (user.isShiftKeyDown()) {
+            if (!user.level().isClientSide()){
+                user.displayClientMessage(Component.literal("jump"), true);
+                AirJump.execute(user, x, y, z);
+            }
+        } else if (!user.isShiftKeyDown()) {
+            if (!user.level().isClientSide()){
+                InteractionHand pHand = user.swingingArm;
+                Item myitem = user.getItemInHand(pHand).getItem();
+                user.displayClientMessage(Component.literal("You have: " + I18n.get(myitem.getDescriptionId())), true);
+//                if ((user.getItemInHand(pHand)).getItem() == Items.IRON_BOOTS){
+//                    user.displayClientMessage(Component.literal("shot"), true);
+//                    DropShotKick.execute(world, user);
+//                } else if ((user.getItemInHand(pHand)).getItem() == Items.DIAMOND_SWORD) {
+//                    user.displayClientMessage(Component.literal("slash"), true);
+//                    ArcSlash.execute(world, user, x, y, z);
+//                }
+            }
+        }
     }
 }
---
+++ src/main/java/com/kkrotello/setofskills/skill/AirJump.java
@@ -0,0 +1,13 @@
+package com.kkrotello.setofskills.skill;
+
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.phys.Vec3;
+
+public class AirJump {
+    public static void execute(Player user, double x, double y, double z) {
+        if (user == null)
+            return;
+        user.setDeltaMovement(new Vec3((user.getLookAngle().x), (user.getLookAngle().y)*1.5, (user.getLookAngle().z)));
+        
+    }
+}
